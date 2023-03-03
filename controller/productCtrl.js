8ce2fb32b8faeca7ab0e9ab5ebda1390fa7631fe
ShitalUnde <unde.shital1995@gmail.com>
2023-03-03 19:56:24 +0530

    code push

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,42 +1,75 @@
-const asyncHandler = require('express-async-handler')
-const Product = require('../models/productModel')
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+const slugify = require("slugify");
+const {validateMongoDBId} = require('../utils/validateMongodbId.js')
 
-const createProduct = asyncHandler(async (req,res) => {
-try{
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const newProduct = await Product.create(req.body);
+    res.json({ newProduct });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-    const newProduct = await Product.create(req.body)
-    res.json({newProduct})
-}
-catch(error) {
-    throw new Error(error)
-}
-})
+const getProductById = asyncHandler(async (req, res) => {
+  console.log(req.params);
+  try {
+    const findProduct = await Product.findById(req.params.id);
+    if (!findProduct) throw new Error("Product not available for this Id");
+    res.json(findProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-const getProductById = asyncHandler(async (req,res) => {
-    console.log(req.params)
+const getAllProduct = asyncHandler(async (req, res) => {
+  const getAllProduct = await Product.find();
+  console.log(getAllProduct);
+  if (!getAllProduct) throw new Error("There is no product");
+
+  res.send({
+    data: getAllProduct,
+    meta: {
+      totalRecordFound: await Product.find().count(),
+    },
+  });
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+      const updateProduct = await Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.json(updateProduct);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteProduct = asyncHandler(async (req,res) => {
+    const {id} = req.params
+        validateMongoDBId(id)
     try{
-        const findProduct = await Product.findById(req.params.id)
-        if(!findProduct)
-        throw new Error('Product not available for this Id')
-        res.json(findProduct)
+        // const findProduct
     }
-    catch(error){
-        throw new Error(error)
+    catch(error) {
+       throw new Error(error) 
     }
 })
 
-const getAllProduct = asyncHandler(async (req,res) => {
-    const getAllProduct = await Product.find()
-    console.log(getAllProduct)
-    if(!getAllProduct)
-    throw new Error('There is no product')
-
-    res.send({
-        data: getAllProduct,
-        meta: {
-            totalRecordFound: await Product.find().count(),
-        }
-    })
-})
-
-module.exports = {createProduct, getProductById, getAllProduct}
+module.exports = {
+  createProduct,
+  getProductById,
+  getAllProduct,
+  updateProduct,
+  deleteProduct
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const {createProduct, getProductById, getAllProduct} = require('../controller/productCtrl')
+const {createProduct, getProductById, getAllProduct, updateProduct, deleteProduct} = require('../controller/productCtrl')
 const { authHandler, isAdmin } = require('../middlewares/auth')
 
 
 router.post('/create-product', authHandler,isAdmin, createProduct)
 router.get('/getAllProduct', authHandler,isAdmin, getAllProduct)
-router.get('/find-by-id/:id', authHandler,isAdmin, getProductById)
+router.get('/:id', authHandler,isAdmin, getProductById)
 
+router.put('/:id', authHandler,isAdmin, updateProduct)
+router.delete('/:id', authHandler,isAdmin, deleteProduct)
+
+
+/* 
+IMP*
+
+ 
+diff bet PUT & PATCH
+put is used to replace full Object
+wehere as , Patch is used to update specific value only*/
 
 module.exports = router
